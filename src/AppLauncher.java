bbce2ad507b0a892bee5c3d623182614a31a0cc1
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-19 22:25:41 +0800

    ref: Re-estalished the code to actually match the logic

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,22 +1,12 @@
+import view.ConsoleView;
 
 public class AppLauncher {
 	
-	public static void DisplayMenu() {
-		
-		System.out.println("Welcome to Animal Shelter System." + "\n" +
-		"[1] View All" + "\n" +
-	    "[2] Add Animal" + "\n" + 
-		"[3] Search by Name \n" + 
-	    "[4] Update Animal \n" + 
-		"[5] Exit");
-		 
-	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
-		DisplayMenu();
+		ConsoleView.DisplayMenu();
 	}
 
 }
