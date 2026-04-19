aba410458b3676cc4c573fb87b050af58e673c16
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-19 21:58:33 +0800

    launch: AppLauncher file for displaying the available features to users.

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,9 +1,22 @@
 
 public class AppLauncher {
-
+	
+	public static void DisplayMenu() {
+		
+		System.out.println("Welcome to Animal Shelter System." + "\n" +
+		"[1] View All" + "\n" +
+	    "[2] Add Animal" + "\n" + 
+		"[3] Search by Name \n" + 
+	    "[4] Update Animal \n" + 
+		"[5] Exit");
+		 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		DisplayMenu();
 	}
 
 }
