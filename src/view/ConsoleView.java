a14b1ca44c08212d9e8a0c102498ee19d0ed41ae
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-20 23:36:06 +0800

    fix: jline remove because of terminal compatibility.

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -7,7 +7,7 @@ import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.InfoCmp.Capability;
+import org.jline.utils.InfoCmp;
 import org.jline.utils.NonBlockingReader;
 
 import view.ViewUtil;
@@ -16,51 +16,18 @@ public class ConsoleView {
 	
 	 public static int DisplayOption() {
 
-		 String[] options = { "View All", "Add Animal", "Search by Name", "" };
-         int selectedIndex = 0;
+		 System.out.println("Welcome to Animal Shelter System." + "\n" +
+				 "[1] View All" + "\n" +
+				 "[2] Add Animal" + "\n" +
+				 "[3] Search by Name \n" +
+				 "[4] Update Animal \n" +
+				 "[5] Exit");
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("Enter your choice: ");
 
-		 try(Terminal terminal = TerminalBuilder.builder().system(true).build()) {
-          terminal.enterRawMode();
-		  NonBlockingReader reader = terminal.reader();
+		 int choice = scan.nextInt();
 
-		  while(true) {
-			  terminal.puts(Capability.clear_screen);
-			  terminal.flush();
-
-			  for (int i= 0; i < options.length; i++) {
-                  if (selectedIndex == i) {
-                     System.out.println("> " + options[i] + " <");
-				  } else {
-					  System.out.println("  " + options[i]);
-				  }
-			  }
-
-			  int read = reader.read();
-
-			  if (read == 13 || read == 10) {
-				  return selectedIndex + 1;
-			  }
-
-			  if (read == 27) {
-				  int next1 = reader.read();
-				  int next2 = reader.read();
-
-				  if(next1 == '[') {
-                      if (next2 == 'A') {
-						  selectedIndex = (selectedIndex > 0) ? selectedIndex - 1 : options.length - 1;
-					  } else {
-						  selectedIndex = (selectedIndex < options.length - 1) ? selectedIndex + 1 : 0;
-					  }
-				  }
-			  }
-
-		  }
-		  }
-
-		 catch(IOException e) {
-			 e.printStackTrace();
-			 return -1;
-		 }
+		 return choice;
 	 }
    
      public static void DisplayMenu() {
