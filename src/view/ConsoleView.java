3d5f85bcd2de170c7d9bc015630ab1466f0827a9
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2026-04-20 23:16:45 +0800

    init: Building of option selection.

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,24 +1,66 @@
 package view;
+import java.io.IOException;
 import java.util.*;
 import model.AnimalModel;
 import com.shelter.model.*;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.InfoCmp.Capability;
+import org.jline.utils.NonBlockingReader;
 
 import view.ViewUtil;
 
 public class ConsoleView {
 	
 	 public static int DisplayOption() {
-		 System.out.println("Welcome to Animal Shelter System." + "\n" +
-					"[1] View All" + "\n" +
-				    "[2] Add Animal" + "\n" + 
-					"[3] Search by Name \n" + 
-				    "[4] Update Animal \n" + 
-					"[5] Exit");
-		    Scanner scan = new Scanner(System.in);
-			System.out.println("Enter your choice: ");
-			int choice = scan.nextInt();
-			
-			return choice;
+
+		 String[] options = { "View All", "Add Animal", "Search by Name", "" };
+         int selectedIndex = 0;
+
+		 try(Terminal terminal = TerminalBuilder.builder().system(true).build()) {
+          terminal.enterRawMode();
+		  NonBlockingReader reader = terminal.reader();
+
+		  while(true) {
+			  terminal.puts(Capability.clear_screen);
+			  terminal.flush();
+
+			  for (int i= 0; i < options.length; i++) {
+                  if (selectedIndex == i) {
+                     System.out.println("> " + options[i] + " <");
+				  } else {
+					  System.out.println("  " + options[i]);
+				  }
+			  }
+
+			  int read = reader.read();
+
+			  if (read == 13 || read == 10) {
+				  return selectedIndex + 1;
+			  }
+
+			  if (read == 27) {
+				  int next1 = reader.read();
+				  int next2 = reader.read();
+
+				  if(next1 == '[') {
+                      if (next2 == 'A') {
+						  selectedIndex = (selectedIndex > 0) ? selectedIndex - 1 : options.length - 1;
+					  } else {
+						  selectedIndex = (selectedIndex < options.length - 1) ? selectedIndex + 1 : 0;
+					  }
+				  }
+			  }
+
+		  }
+		  }
+
+		 catch(IOException e) {
+			 e.printStackTrace();
+			 return -1;
+		 }
 	 }
    
      public static void DisplayMenu() {
